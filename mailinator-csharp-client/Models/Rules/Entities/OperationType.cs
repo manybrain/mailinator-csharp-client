7f9f3d1296d06d416fe31d98020038b8031d73e4
Marian Melnychuk <marian.melnychuk@gmail.com>
2023-11-16 18:05:24 +0200

    Add new operation type STARTS_WITH to rules

--- mailinator-csharp-client/Models/Rules/Entities/OperationType.cs
+++ mailinator-csharp-client/Models/Rules/Entities/OperationType.cs
@@ -13,6 +13,9 @@ namespace mailinator_csharp_client.Models.Rules.Entities
         /// Matches when the field (e.g. "to") starts with a string (e.g. "test" matches "test", "test1", "test9999")
         /// </summary>
         [EnumMember(Value = "PREFIX")]
-        PREFIX
+        PREFIX,
+
+        [EnumMember(Value = "STARTS_WITH")]
+        STARTS_WITH
     }
 }
